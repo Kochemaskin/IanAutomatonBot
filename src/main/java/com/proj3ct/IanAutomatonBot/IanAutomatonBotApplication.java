83b412e65ad73add1e5e24010e235f88e62188a5
Dennis <57582915+Dennis824@users.noreply.github.com>
2022-08-19 16:49:35 -0400

    Initial commit

---
+++ src/main/java/com/proj3ct/IanAutomatonBot/IanAutomatonBotApplication.java
@@ -0,0 +1,13 @@
+package com.proj3ct.IanAutomatonBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanAutomatonBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanAutomatonBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proj3ct/IanAutomatonBot/IanAutomatonBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj3ct.IanAutomatonBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanAutomatonBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
