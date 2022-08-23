d0c8aab14d807dc40e75cc3255a12c3d076ecbcb
Dennis <57582915+Dennis824@users.noreply.github.com>
2022-08-23 18:00:17 -0400

    feat. bot work with /start command.

--- src/main/java/com/proj3ct/IanAutomatonBot/IanAutomatonBotApplication.java
+++ src/main/java/com/proj3ct/IanAutomatonBot/IanAutomatonBotApplication.java
@@ -2,8 +2,9 @@ package com.proj3ct.IanAutomatonBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class IanAutomatonBotApplication {
 
 	public static void main(String[] args) {
